c4b7d519ef357cf6cddbdc8575e60c4bd7304dd1
reignstar <faithnyongsn@gmail.com>
2023-05-02 11:13:29 -0500

    shopping cart with react

---
+++ src/products.js
@@ -0,0 +1,60 @@
+import product1 from "./assets/1.jpg";
+import product2 from "./assets/2.png";
+import product3 from "./assets/3.jpg";
+import product4 from "./assets/4.jpg";
+import product5 from "./assets/5.jpg";
+import product6 from "./assets/6.jpg";
+import product7 from "./assets/7.jpg";
+import product8 from "./assets/8.jpg";
+
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Football",
+        price:100.0,
+        productImage: product1
+    },
+    {
+        id: 2,
+        productName: "Basketball",
+        price:160.0,
+        productImage: product2
+    },
+    {
+        id: 3,
+        productName: "Netball",
+        price:178.0,
+        productImage: product3
+    },
+    {
+        id: 4,
+        productName: "Handball",
+        price:210.0,
+        productImage: product4
+    },
+    {
+        id: 5,
+        productName: "Voleyball",
+        price:280.0,
+        productImage: product5
+    },
+    {
+        id: 6,
+        productName: "Tennisball",
+        price:340.0,
+        productImage: product6
+    },
+    {
+        id: 7,
+        productName: "Golfball",
+        price:190.0,
+        productImage: product7
+    },
+    {
+        id: 8,
+        productName: "Hockeyball",
+        price:120.0,
+        productImage: product8
+    },
+];
